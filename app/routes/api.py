7c635ec127c73f13a86a39b699083bc678939368
SidneyW <sidneywijngaarde@gmail.com>
2017-03-05 23:46:47 -0500

    Sorry, wrong branch

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,7 +1,12 @@
 """ ROUTES """
+from pymongo import MongoClient
 from flask import Blueprint, jsonify, request
+import os
 
 blueprint = Blueprint('/api', __name__, url_prefix='/api')
+
+mongo = MongoClient(os.getenv("MONGODB_URI", "mongodb://localhost:27017/"))
+db = mongo["pathi"]
 
 @blueprint.route('/test', methods=['POST'])
 def test():
